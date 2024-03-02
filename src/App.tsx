9ae3d78f7169d54e164acea83adfaaf92ce4892b
gldaniil <daniil.gajfullin@yandex.ru>
2024-03-02 17:04:44 +0500

    Добавляю страницу Home и логику её отображения

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./App.module.scss";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 const App: React.FC = () => {
   const [path, setPath] = useState("/");
@@ -14,45 +15,7 @@ const App: React.FC = () => {
   return (
     <>
       <Header path={path} onClick={handleNavigationClick} />
-      <main className={styles.main}>
-        <section className={styles.intro__wrapper}>
-          <div className={styles.intro__container}>
-            <div className={styles.intro__inner}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi,
-              consequatur autem vitae sapiente consectetur ratione placeat
-              necessitatibus quod inventore! Blanditiis sequi ratione ad vitae
-              totam officiis illum iure atque quia magni! Molestiae molestias
-              aliquid error suscipit quam neque consectetur, voluptas ad. Illo
-              vero a aspernatur unde at itaque numquam veniam dolorem
-              consectetur, atque optio ullam nulla iusto tenetur doloribus nisi
-              commodi praesentium error! Quia libero magni incidunt nulla,
-              commodi accusamus minus ea esse. Eligendi aperiam soluta voluptate
-              aliquid consequatur, sunt neque, ex quam alias ut repudiandae,
-              vitae modi! Ratione, facilis?
-            </div>
-          </div>
-        </section>
-        <section className={styles.news__wrapper}>
-          <div
-            className={styles.news__container}
-            onClick={handleNavigationClick}
-          >
-            <div className={styles.news__inner}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi,
-              consequatur autem vitae sapiente consectetur ratione placeat
-              necessitatibus quod inventore! Blanditiis sequi ratione ad vitae
-              totam officiis illum iure atque quia magni! Molestiae molestias
-              aliquid error suscipit quam neque consectetur, voluptas ad. Illo
-              vero a aspernatur unde at itaque numquam veniam dolorem
-              consectetur, atque optio ullam nulla iusto tenetur doloribus nisi
-              commodi praesentium error! Quia libero magni incidunt nulla,
-              commodi accusamus minus ea esse. Eligendi aperiam soluta voluptate
-              aliquid consequatur, sunt neque, ex quam alias ut repudiandae,
-              vitae modi! Ratione, facilis?
-            </div>
-          </div>
-        </section>
-      </main>
+      <main className={styles.main}>{path === "/" && <Home />}</main>
       <footer style={{ flex: "0 0 auto" }}>3</footer>
     </>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <section className={styles.intro__wrapper}>
+        <div className={styles.intro__container}>
+          <div className={styles.intro__inner}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi,
+            consequatur autem vitae sapiente consectetur ratione placeat
+            necessitatibus quod inventore! Blanditiis sequi ratione ad vitae
+            totam officiis illum iure atque quia magni! Molestiae molestias
+            aliquid error suscipit quam neque consectetur, voluptas ad. Illo
+            vero a aspernatur unde at itaque numquam veniam dolorem consectetur,
+            atque optio ullam nulla iusto tenetur doloribus nisi commodi
+            praesentium error! Quia libero magni incidunt nulla, commodi
+            accusamus minus ea esse. Eligendi aperiam soluta voluptate aliquid
+            consequatur, sunt neque, ex quam alias ut repudiandae, vitae modi!
+            Ratione, facilis?
+          </div>
+        </div>
+      </section>
+      <section className={styles.news__wrapper}>
+        <div className={styles.news__container}>
+          <div className={styles.news__inner}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi,
+            consequatur autem vitae sapiente consectetur ratione placeat
+            necessitatibus quod inventore! Blanditiis sequi ratione ad vitae
+            totam officiis illum iure atque quia magni! Molestiae molestias
+            aliquid error suscipit quam neque consectetur, voluptas ad. Illo
+            vero a aspernatur unde at itaque numquam veniam dolorem consectetur,
+            atque optio ullam nulla iusto tenetur doloribus nisi commodi
+            praesentium error! Quia libero magni incidunt nulla, commodi
+            accusamus minus ea esse. Eligendi aperiam soluta voluptate aliquid
+            consequatur, sunt neque, ex quam alias ut repudiandae, vitae modi!
+            Ratione, facilis?
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
