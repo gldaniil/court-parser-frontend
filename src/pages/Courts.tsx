2c2f355c8b46a35527ff136b419b4748459baa75
gldaniil <daniil.gajfullin@yandex.ru>
2024-03-08 20:45:45 +0500

    Добавляю состояние для загрузки контента (списка судов)

--- src/pages/Courts.tsx
+++ src/pages/Courts.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import ContentLoader from "react-content-loader";
 import PageProps from "../types/PageProps";
 import styles from "./Courts.module.scss";
+import List from "../components/List";
 
 const Courts: React.FC<PageProps> = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <section className={styles.courts__wrapper}>
       <div className={styles.courts__container}>
@@ -10,7 +14,20 @@ const Courts: React.FC<PageProps> = () => {
           <h1 className={styles.courts__title}>Суды</h1>
           <h2 className={styles.courts__subtitle}>Список доступных судов</h2>
           <div className={styles.courts__content}>
-            Здесь будет список судов...
+            {isLoading ? (
+              <List />
+            ) : (
+              <ContentLoader
+                speed={3}
+                interval={0}
+                width={"100%"}
+                height={500}
+                backgroundColor="#16171d"
+                foregroundColor="#101114"
+              >
+                <rect x="0" y="0" rx="10" ry="10" width="100%" height="500" />
+              </ContentLoader>
+            )}
           </div>
           <div className={styles.faq}>
             <h3 className={styles.faq__title}>Дополнительные сведения</h3>
