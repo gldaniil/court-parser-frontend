48fd279da6af12c4a1bacf868ce52e49a6acc109
gldaniil <daniil.gajfullin@yandex.ru>
2024-03-10 23:43:07 +0500

    Добавляю типизацию компонента List

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,5 +1,14 @@
-const List: React.FC = () => {
-  return <div>List</div>;
+type ListProps = {
+  courts: {
+    id: number;
+    court_name: string;
+    url: string;
+    last_changed: string;
+  }[];
+};
+
+const List: React.FC<ListProps> = ({ courts }) => {
+  return <div>Test</div>;
 };
 
 export default List;
